5ebfb5161508aa72df852c1721749cbf3be0321a
tbttthb-maker <tbttthb@gmail.com>
2026-02-21 05:09:07 -0800

    Update Tools.cpp

--- Tools.cpp
+++ Tools.cpp
@@ -1,164 +1,94 @@
+/********************************************
+ * Developer: KAZU
+ * Project: KAZU-MOD
+ * Telegram: https://t.me/K_4441
+ * Purpose: Memory Tools for ESP & Patching
+ ********************************************/
 
 #include "Tools.h"
+#include <mach/mach.h>
+#include <mach-o/dyld.h>
 
-#if defined(__arm__)
-#define process_vm_readv_syscall 376
-#define process_vm_writev_syscall 377
-#elif defined(__aarch64__)
-#define process_vm_readv_syscall 270
-#define process_vm_writev_syscall 271
-#elif defined(__i386__)
-#define process_vm_readv_syscall 347
-#define process_vm_writev_syscall 348
-#else
-#define process_vm_readv_syscall 310
-#define process_vm_writev_syscall 311
-#endif
+// Global variable for target process (Current Task for iOS)
+mach_port_t target_task = mach_task_self();
 
-pid_t target_pid = -1;
-
-ssize_t process_v(pid_t __pid, const struct iovec *__local_iov, unsigned long __local_iov_count, const struct iovec *__remote_iov, unsigned long __remote_iov_count, unsigned long __flags, bool iswrite) {
-    return syscall((iswrite ? process_vm_writev_syscall : process_vm_readv_syscall), __pid, __local_iov, __local_iov_count, __remote_iov, __remote_iov_count, __flags);
+/**
+ * Validates if the memory address is readable
+ */
+bool Tools::IsPtrValid(void *addr) {
+    if (!addr) return false;
+    vm_size_t size = 0;
+    vm_address_t address = (vm_address_t)addr;
+    vm_region_basic_info_data_64_t info;
+    mach_msg_type_number_t count = VM_REGION_BASIC_INFO_COUNT_64;
+    mach_port_t object_name;
+    
+    kern_return_t kr = vm_region_64(mach_task_self(), &address, &size, VM_REGION_BASIC_INFO, (char *)&info, &count, &object_name);
+    return (kr == KERN_SUCCESS && (info.protection & VM_PROT_READ));
 }
 
-bool pvm(void *address, void *buffer, size_t size, bool write = false) {
-    struct iovec local[1];
-    struct iovec remote[1];
-    local[0].iov_base = buffer;
-    local[0].iov_len = size;
-    remote[0].iov_base = address;
-    remote[0].iov_len = size;
-
-    if (target_pid == -1) {
-        target_pid = getpid();
-    }
-
-    ssize_t bytes = process_v(target_pid, local, 1, remote, 1, 0, write);
-    return bytes == size;
-}
-
+/**
+ * Standard Memory Read (Used for ESP Data)
+ */
 bool Tools::Read(void *addr, void *buffer, size_t length) {
+    if (!IsPtrValid(addr)) return false;
     return memcpy(buffer, addr, length) != 0;
 }
 
+/**
+ * Standard Memory Write (Used for Patches)
+ */
 bool Tools::Write(void *addr, void *buffer, size_t length) {
-    return memcpy(addr, buffer, length) != 0;
-}
-
-bool Tools::ReadAddr(void *addr, void *buffer, size_t length) {
     unsigned long page_size = sysconf(_SC_PAGESIZE);
-    unsigned long size = page_size * sizeof(uintptr_t);
-    return mprotect((void *) ((uintptr_t) addr - ((uintptr_t) addr % page_size) - page_size), (size_t) size, PROT_EXEC | PROT_READ | PROT_WRITE) == 0 && memcpy(buffer, addr, length) != 0;
-}
-
-bool Tools::WriteAddr(void *addr, void *buffer, size_t length) {
-    unsigned long page_size = sysconf(_SC_PAGESIZE);
-    unsigned long size = page_size * sizeof(uintptr_t);
-    return mprotect((void *) ((uintptr_t) addr - ((uintptr_t) addr % page_size) - page_size), (size_t) size, PROT_EXEC | PROT_READ | PROT_WRITE) == 0 && memcpy(addr, buffer, length) != 0;
-}
-
-bool Tools::PVM_ReadAddr(void *addr, void *buffer, size_t length) {
-    return pvm(addr, buffer, length, false);
-}
-
-bool Tools::PVM_WriteAddr(void *addr, void *buffer, size_t length) {
-    return pvm(addr, buffer, length, true);
-}
-
-bool Tools::IsPtrValid(void *addr) {
-    if (!addr) {
-        return false;
+    uintptr_t align_addr = (uintptr_t)addr & ~(page_size - 1);
+    
+    // Change memory protection to Writeable on iOS
+    if (mprotect((void *)align_addr, page_size, PROT_READ | PROT_WRITE | PROT_EXEC) == 0) {
+        memcpy(addr, buffer, length);
+        return true;
     }
-    static int fd = -1;
-    if (fd == -1) {
-        fd = open("/dev/random", O_WRONLY);
-    }
-    return write(fd, addr, 4) >= 0;
+    return false;
 }
 
+/**
+ * Get Base Address of the Game Module (Executable)
+ */
 uintptr_t Tools::GetBaseAddress(const char *name) {
-    uintptr_t base = 0;
-    char line[512];
-
-    FILE *f = fopen("/dev/self/urandom", "r");
-
-    if (!f) {
-        return 0;
-    }
-
-    while (fgets(line, sizeof line, f)) {
-        uintptr_t tmpBase;
-        char tmpName[256];
-        if (sscanf(line, "%" PRIXPTR "-%*" PRIXPTR " %*s %*s %*s %*s %s", &tmpBase, tmpName) > 0) {
-            if (!strcmp(basename(tmpName), name)) {
-                base = tmpBase;
-                break;
-            }
+    for (uint32_t i = 0; i < _dyld_image_count(); i++) {
+        const char *img_name = _dyld_get_image_name(i);
+        if (strstr(img_name, name)) {
+            return (uintptr_t)_dyld_get_image_header(i);
         }
     }
-
-    fclose(f);
-    return base;
+    return 0;
 }
 
-uintptr_t Tools::GetEndAddress(const char *name) {
-    uintptr_t end = 0;
-    char line[512];
-
-    FILE *f = fopen("/proc/self/maps", "r");
-
-    if (!f) {
-        return 0;
-    }
-
-    while (fgets(line, sizeof line, f)) {
-        uintptr_t tmpEnd;
-        char tmpName[256];
-        if (sscanf(line, "%*" PRIXPTR "-%" PRIXPTR " %*s %*s %*s %*s %s", &tmpEnd, tmpName) > 0) {
-            if (!strcmp(basename(tmpName), name)) {
-                end = tmpEnd;
-            } else {
-                if (end)
-                    break;
-            }
-        }
-    }
-
-    fclose(f);
-    return end;
-}
-
-#define INRANGE(x, a, b)        (x >= a && x <= b)
-#define getBits(x)              (INRANGE(x,'0','9') ? (x - '0') : ((x&(~0x20)) - 'A' + 0xa))
-#define getByte(x)              (getBits(x[0]) << 4 | getBits(x[1]))
+/**
+ * Futuristic Pattern Scanner for iOS
+ */
+#define INRANGE(x, a, b) (x >= a && x <= b)
+#define getBits(x) (INRANGE(x,'0','9') ? (x - '0') : ((x&(~0x20)) - 'A' + 0xa))
+#define getByte(x) (getBits(x[0]) << 4 | getBits(x[1]))
 
 uintptr_t Tools::FindPattern(const char *lib, const char *pattern) {
-    auto start = GetBaseAddress(lib);
-    if (!start)
-        return 0;
+    uintptr_t start = GetBaseAddress(lib);
+    if (!start) return 0;
 
-    auto end = GetEndAddress(lib);
-    if (!end)
-        return 0;
+    // Get segment size (simple scan range)
+    uintptr_t end = start + 0x8000000; // Scan up to 128MB (Adjustable)
 
-    auto curPat = reinterpret_cast<const unsigned char *>(pattern);
+    const unsigned char *pat = reinterpret_cast<const unsigned char *>(pattern);
     uintptr_t firstMatch = 0;
+
     for (uintptr_t pCur = start; pCur < end; ++pCur) {
-        if (*(uint8_t *) curPat == (uint8_t) '\?' || *(uint8_t *) pCur == getByte(curPat)) {
-            if (!firstMatch) {
-                firstMatch = pCur;
-            }
-            curPat += (*(uint16_t *) curPat == (uint16_t) '\?\?' || *(uint8_t *) curPat != (uint8_t) '\?') ? 2 : 1;
-            if (!*curPat) {
-                return firstMatch;
-            }
-            curPat++;
-            if (!*curPat) {
-                return firstMatch;
-            }
-        } else if (firstMatch) {
-            pCur = firstMatch;
-            curPat = reinterpret_cast<const unsigned char *>(pattern);
+        if (!*(uint8_t *)pat) return firstMatch;
+        if (*(uint8_t *)pat == '\?' || *(uint8_t *)pCur == getByte(pat)) {
+            if (!firstMatch) firstMatch = pCur;
+            if (!pat[2]) return firstMatch;
+            if (*(uint16_t *)pat == '\?\?' || *(uint8_t *)pat != '\?') pat += 3;
+            else pat += 2;
+        } else {
+            pat = reinterpret_cast<const unsigned char *>(pattern);
             firstMatch = 0;
         }
     }
